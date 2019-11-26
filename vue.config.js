114bb23d4a63c6dbd9369b2b2e279f729d30beb5
thenry21 <thenry21k@gmail.com>
2019-11-26 20:33:29 +0900

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,26 @@
 module.exports = {
   'transpileDependencies': [
     'vuetify'
-  ]
+  ],
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        appId: 'kr.codebee.ve_youtube',
+        nsis: {
+          shortcutName: 'first_app'
+        },
+        win: {
+          target: [
+            {
+              target: "nsis",
+              arch: [
+                'x64',
+                'ia32'
+              ]
+            }
+          ]
+        }
+      }
+    }
+  }
 }
